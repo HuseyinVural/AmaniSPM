ba46c2626aa158ead15295faf83b5825e3e518b3
Huseyin Vural <huseyin.vural@live.com>
2023-06-13 18:15:01 +0300

    Update SDK version.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AmaniSPM",
-            targets: ["Amani"])
+            targets: ["AmaniVideoSDK"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "15.0.0")),
@@ -20,8 +21,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "Amani",
-            path: "./Amani.xcframework"
+            name: "AmaniVideoSDK",
+            path: "./AmaniVideoSDK.xcframework"
         )
     ]
 )
